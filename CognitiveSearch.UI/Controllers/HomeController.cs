af8b99171b75a65790ef7641e11f183990243e45
jcbendernh <jcbender@hotmail.com>
2019-06-05 17:53:00 -0400

    Add project files.

---
+++ CognitiveSearch.UI/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Auth;
+using CognitiveSearch.UI.Models;
+
+namespace CognitiveSearch.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration { get; set; }
+        private DocumentSearchClient _docSearch { get; set; }
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _docSearch = new DocumentSearchClient(configuration);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string q)
+        {
+            var searchidId = _docSearch.GetSearchId().ToString();
+
+            if (searchidId != string.Empty)
+                TempData["searchId"] = searchidId;
+
+            TempData["query"] = q;
+            TempData["applicationInstrumentationKey"] = _configuration.GetSection("InstrumentationKey")?.Value;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetDocuments(string q = "", SearchFacet[] searchFacets = null, int currentPage = 1)
+        {
+            var token = GetContainerSasUri();
+            var selectFilter = _docSearch.Model.SelectFilter;
+
+            if (!string.IsNullOrEmpty(q))
+            {
+                q = q.Replace("-", "").Replace("?", "");
+            }
+
+            var response = _docSearch.Search(q, searchFacets, selectFilter, currentPage);
+            var facetResults = new List<object>();
+            var tagsResults = new List<object>();
+
+            if (response!= null && response.Facets != null)
+            {
+                // Return only the selected facets from the Search Model
+                foreach (var facetResult in response.Facets.Where(f => _docSearch.Model.Facets.Where(x => x.Name == f.Key).Any()))
+                {
+                    facetResults.Add(new
+                    {
+                        key = facetResult.Key,
+                        value = facetResult.Value
+                    });
+                }
+
+                foreach (var tagResult in response.Facets.Where(t => _docSearch.Model.Tags.Where(x => x.Name == t.Key).Any()))
+                {
+                    tagsResults.Add(new
+                    {
+                        key = tagResult.Key,
+                        value = tagResult.Value
+                    });
+                }
+            }
+
+            return new JsonResult(new DocumentResult { Results = response.Results, Facets = facetResults, Tags = tagsResults, Count = Convert.ToInt32(response.Count), Token = token });
+        }
+
+        [HttpPost]
+        public IActionResult GetDocumentById(string id = "")
+        {
+            var token = GetContainerSasUri();
+
+            var response = _docSearch.LookUp(id);
+            var facetResults = new List<object>();
+
+            return new JsonResult(new DocumentResult { Result = response, Token = token });
+        }
+
+        private string GetContainerSasUri()
+        {
+            return "<insert SAS token value here>";
+            //return "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rl&se=2020-11-07T01:22:33Z&st=2018-11-06T17:22:33Z&spr=https&sig=pzQF2LEt3m%2B6xizR5u9DiQfIEnP8B5ZViHerslqsCLk%3D";
+            //string sasContainerToken;
+            //string accountName = _configuration.GetSection("StorageAccountName")?.Value;
+            //string accountKey = _configuration.GetSection("StorageAccountKey")?.Value;
+            //string containerAddress = _configuration.GetSection("StorageContainerAddress")?.Value;
+            //CloudBlobContainer container = new CloudBlobContainer(new Uri(containerAddress), new StorageCredentials(accountName, accountKey));
+
+            //SharedAccessBlobPolicy adHocPolicy = new SharedAccessBlobPolicy()
+            //{
+            //    SharedAccessExpiryTime = DateTime.UtcNow.AddHours(1),
+            //    Permissions = SharedAccessBlobPermissions.Read
+            //};
+
+            //sasContainerToken = container.GetSharedAccessSignature(adHocPolicy, "myGroupPolicy");
+            //return sasContainerToken;
+        }
+    }
+}
---
+++ CognitiveSearch.UI/Search/SearchModel.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CognitiveSearch.UI
+{
+    public class SearchModel
+    {
+        private string[] facets = new string[]
+        {
+            // Add UI facets here in order
+            "text",
+            "layoutText"
+            //"keyPhrases"
+        };
+
+        private string[] tags = new string[]
+        {
+            // Add tags fields here in order
+            "text",
+            "layoutText"
+        };
+
+        private string[] resultFields = new string[]
+        {
+            // Add fields needed to display results cards
+            "content",
+            "metadata_storage_path",
+            "metadata_storage_name",
+            "text",
+            "layoutText"
+            //"keyPhrases"
+        };
+
+        public List<SearchField> Facets { get; set; }
+        public List<SearchField> Tags { get; set; }
+
+        public string[] SelectFilter { get; set; }
+
+        public Dictionary<string, string[]> SearchFacets { get; set; }
+
+        public SearchModel(SearchSchema schema)
+        {
+            Facets = new List<SearchField>();
+            Tags = new List<SearchField>();
+            SelectFilter = resultFields;
+
+            if (facets.Count() > 0)
+            {
+                // add field to facets if in facets arr
+                foreach (var field in facets)
+                {
+                    if (schema.Fields.ContainsKey(field) && schema.Fields[field] != null && schema.Fields[field].IsFacetable)
+                    {
+                        Facets.Add(schema.Fields[field]);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var field in schema.Fields.Where(f => f.Value.IsFacetable))
+                {
+                    Facets.Add(field.Value);
+                }
+            }
+
+            if (tags.Count() > 0)
+            {
+                foreach (var field in tags)
+                {
+                    if (schema.Fields[field] != null && schema.Fields[field].IsFacetable)
+                    {
+                        Tags.Add(schema.Fields[field]);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var field in schema.Fields.Where(f => f.Value.IsFacetable))
+                {
+                    Tags.Add(field.Value);
+                }
+            }
+        }
+    }
+}
